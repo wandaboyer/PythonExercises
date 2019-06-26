251cc124606304f2afd48efec898d773b6662aac
kabamwboyer <wboyer@kabaminc.com>
2019-06-26 09:06:21 -0700

    using bitarray to implement barrier

--- boolean_barrier.py
+++ boolean_barrier.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#from threading import Thread, Semaphore, BoundedSemaphore
 import sys
 import threading
 from time import time
@@ -9,10 +8,14 @@ NUM_THREADS = 10
 
 mutex = threading.BoundedSemaphore(1)
 thread_count = 0
-barrier = threading.Semaphore(0)
+count_barrier = threading.Semaphore(0)
 
-boolean_barrier = threading.BoundedSemaphore(0)
-boolean_thread_completion = bitarray(2 ** NUM_THREADS)
+boolean_barrier = threading.Semaphore(0)
+boolean_thread_completion = bitarray(NUM_THREADS)
+boolean_thread_completion.setall(0)
+boolean_mask = bitarray(NUM_THREADS)
+boolean_mask.setall(1)
+
 
 def barrier(barrier_type='count'):
     sys.stdout.write('\nBefore Spawning Threads\n')
@@ -20,7 +23,7 @@ def barrier(barrier_type='count'):
     def count_barrier_proc():
         global mutex
         global thread_count
-        global barrier
+        global count_barrier
         thread_name = threading.currentThread().getName()
 
         mutex.acquire()
@@ -30,9 +33,9 @@ def barrier(barrier_type='count'):
         sys.stdout.write(f"\n\tThis is thread {thread_name} just before the block")
 
         if thread_count == NUM_THREADS:
-            barrier.release()
-        barrier.acquire()
-        barrier.release()
+            count_barrier.release()
+        count_barrier.acquire()
+        count_barrier.release()
 
         sys.stdout.write(f"\n\t\tThis is thread {thread_name} past the block!\n")
 
@@ -43,21 +46,42 @@ def barrier(barrier_type='count'):
         thread_name = threading.currentThread().getName()
 
         mutex.acquire()
-        thread_count += 1
+        boolean_thread_completion = flip(boolean_thread_completion, thread_name)
         mutex.release()
 
         sys.stdout.write(f"\n\tThis is thread {thread_name} just before the block")
+        
+        if boolean_thread_completion == boolean_mask:
+            boolean_barrier.release()
+        boolean_barrier.acquire()
+        boolean_barrier.release()
 
-        if thread_count == NUM_THREADS:
-            barrier.release()
-        barrier.acquire()
-        barrier.release()
+        sys.stdout.write(f"\n\t\tThis is thread {thread_name} past the block!\n")
 
-        sys.stdout.write(f"\n\t\tThis is thread {thread_n
+    def flip(bitarray, b):
+        """
+            Modified from:
+            https://github.com/Imperium-Software/resolver/blob/master/SATSolver/individual.py
+            Flips the bit at position b.
+        """
+        try:
+            b = int(b)
+        except ValueError as e:
+            raise ValueError(f"The value of b can't be interpreted as an integer; {e}")
+        
+        if b >= bitarray.length() or b < 0:
+            return
+        bitarray[b] = not bitarray[b]
+        return bitarray
 
     threads = []
+    try:
+        thread_method = locals()[f"{barrier_type}_barrier_proc"]
+    except KeyError as e:
+        raise KeyError(f"Don't have a barrier type {barrier_type} to fetch method for; {e}")
+
     for i in range(NUM_THREADS):
-        thread = threading.Thread(name=f'thread_{i}', target=count_barrier_proc)
+        thread = threading.Thread(name=f'{i}', target=thread_method)
         threads.append(thread)
         thread.start()
 
@@ -70,11 +94,7 @@ def barrier(barrier_type='count'):
     sys.stdout.write('\nDone joining threads.\n')
 
 
-
-
-
 if __name__ == "__main__":
-    # want total time of all threads, time for each thread; also how long does it take once you've surmounted the barrier for all the rest of the threads to finish off
     time_before = time()
     barrier(barrier_type='boolean')
     time_after = time()
